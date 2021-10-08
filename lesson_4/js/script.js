033a2511c02d0dba80cd364d7b7fb8d7b571ea39
garrettbadger <garrett.badger.id@gmail.com>
2021-10-08 14:07:35 -0600

    tried to add JS

--- lesson_4/js/script.js
+++ lesson_4/js/script.js
@@ -2,3 +2,8 @@ function toggleMenu(){
     
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    
+const today = new Date();
+document.querySelector("#date").textContent = today.toLocaleDateString('en-UK', options);
