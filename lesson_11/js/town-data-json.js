4bfef902237bc22f96b87087b870a1beaff09b7b
garrettbadger <garrett.badger.id@gmail.com>
2021-11-26 17:48:55 -0700

    added events to each town and created each town

---
+++ lesson_11/js/town-data-json.js
@@ -0,0 +1,66 @@
+const requestURL = 'towndata.json';
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+ 
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < 3; i++ ) {
+        let i2 = 0;
+        let card = document.createElement('section');
+        let data = document.createElement('div')
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let img = document.createElement('img');
+        if (i == 1){
+            i2 = 2;
+        h2.textContent = towns[i2].name;
+        p1.textContent = towns[i2].motto;
+        p2.textContent = 'Year founded: ' + towns[i2].yearFounded;
+        p3.textContent = 'Current population: ' + towns[i2].currentPopulation;
+        p4.textContent = 'Average rainfal: ' + towns[i2].averageRainfall;
+        img.setAttribute('src', 'images/card_fish_haven.jpg');
+        img.setAttribute('alt', h2.textContent);
+        }
+        else if (i == 2){
+            i2 = 6;
+        h2.textContent = towns[i2].name;
+        p1.textContent = towns[i2].motto;
+        p2.textContent = 'Year founded: ' + towns[i2].yearFounded;
+        p3.textContent = 'Current population: ' + towns[i2].currentPopulation;
+        p4.textContent = 'Average rainfal: ' + towns[i2].averageRainfall;
+        img.setAttribute('src', 'images/card_preston.jpg');
+        img.setAttribute('alt', h2.textContent);
+        
+        }
+        else if(i == 0){
+            i2 = 0;
+        h2.textContent = towns[i2].name;
+        p1.textContent = towns[i2].motto;
+        p2.textContent = 'Year founded: ' + towns[i2].yearFounded;
+        p3.textContent = 'Current population: ' + towns[i2].currentPopulation;
+        p4.textContent = 'Average rainfal: ' + towns[i2].averageRainfall;
+        img.setAttribute('src', 'images/card_soda_springs.jpg');
+        img.setAttribute('alt', h2.textContent);
+        }
+
+        data.appendChild(h2);
+        data.appendChild(p1);
+        data.appendChild(p2);
+        data.appendChild(p3);
+        data.appendChild(p4);
+        
+        card.appendChild(img);
+        card.appendChild(data)
+
+        document.querySelector('div.cards').appendChild(card);
+        // document.querySelector('div.cards').appendChild(data);
+        // document.querySelector('div.cards').appendChild(img)
+    }
+  });
---
+++ lesson_11/js/weatherapi-fish-haven.js
@@ -0,0 +1,55 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=3a3f5ad2113c8cab626ffe00ab5469a5&units=imperial'
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        const five = jsObject.list.filter(item => item.dt_txt.includes('18:00:00'));
+        const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+        
+        five.forEach((forecast, index) =>{
+            const day = forecast.dt_txt;
+            let newDay = new Date(day).getDay()
+            const imagesrc = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
+            const desc = forecast.weather[0].description;
+            index += 1;
+            document.querySelector(`.day${index}`).textContent = daysOfWeek[newDay];
+            document.querySelector(`.forecast_weather${index}`).textContent = `${forecast.main.temp.toFixed(0)}°F`;
+            document.querySelector(`.forecast_icon${index}`).setAttribute('src', imagesrc)
+            document.querySelector(`.forecast_icon${index}`).setAttribute('alt', desc)
+        })
+          // note the concatenation
+          // note how we reference the weather array
+        // document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+        // document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        // document.getElementById('icon').setAttribute('alt', desc);
+
+        
+    });
+
+const apiURL2 = 'https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=3a3f5ad2113c8cab626ffe00ab5469a5&units=imperial'
+fetch(apiURL2)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let temp = jsObject.main.temp;
+        const desc = jsObject.weather[0].description;
+        // getting the current conditions
+        document.getElementById('currently').textContent = desc + " " + Math.floor(temp)
+        // getting the max temp
+        const maxTemp = jsObject.main.temp_max
+        document.getElementById('temperature').textContent = Math.floor(maxTemp)
+        // getting the windspeed
+        const windSpeed = jsObject.wind.speed
+        document.getElementById('wind').textContent = windSpeed
+        // getting the humidity
+        const humidity = jsObject.main.humidity
+        document.getElementById('humidity').textContent = humidity
+        // windchill
+        let t = document.getElementById("temperature").innerText;
+    let s = document.getElementById("wind").innerText;
+    let temp1 = Number(t)
+    let speed = Number(s)
+
+    var w = 0.00
+    if (temp1 <= 50 & speed > 3) {var w = 35.74 + (0.6215 * temp1) - (35.75 * (speed ** 0.16)) + (0.4275 * temp1 * (speed ** 0.16))};
+    document.getElementById("windchill").textContent = Math.round(w, 2);
+
+    })
